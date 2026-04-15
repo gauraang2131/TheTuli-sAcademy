0277fe211af37c026a98a84983389f34bcbd5315
gauraang2131 <gauraang2131@gmail.com>
2026-04-16 01:45:46 +0530

    Fix basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/TheTuli-sAcademy' : '',
     images: {
         unoptimized: true
     }
